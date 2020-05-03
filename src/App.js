f9de0d056f2f7565e1e1ec6144bceadfb5c3f0bc
nmashDev <nmashDev@gmail.com>
2020-05-03 03:58:55 +0200

    Changed feature test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye world
+          Changed on the feature branch - test
         </p>
         <a
           className="App-link"
